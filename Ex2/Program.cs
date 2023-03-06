deeddcbdc44592e5ddd37617f4c5d750a865a362
Kattebu <butskoev@tut.by>
2023-03-06 19:05:04 +0300

    ex2

---
+++ Ex2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 43: Написать прогр,которая найдет точку пересечения двух прямых, заданных ур:
+// y=k1*x+b1  y=k2*x+b2 Значения k1 k2 b1 b2 задаются пользователем.
+Console.WriteLine("Введите число k1: ");
+int k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число k2: ");
+int k2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число b1: ");
+int b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число b2: ");
+int b2 = Convert.ToInt32(Console.ReadLine());
+double x=(b2-b1)/(k1-k2);
+double y=k1*(b2-b1)/(k1-k2)+b1;
+if(k1!=k2)
+{
+    Console.WriteLine($"Точка пересечения двух прямых имеет координаты: {x},{y}");
+}
+else
+{
+  Console.WriteLine("Прямые не пересекаются"); 
+} 
