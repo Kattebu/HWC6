300524591bf042e55f65fca2b8c0a1bb6c3e9e43
Kattebu <butskoev@tut.by>
2023-03-06 18:22:44 +0300

    ex1

--- Ex1/Program.cs
+++ Ex1/Program.cs
@@ -1,1 +1,14 @@
 ﻿// Задача 41: Поль-ль вводит М чисел. Сколько чисел больше нуля ввел п-ль.
+Console.Write("Введите M чисел: ");
+string[]array=Console.ReadLine().Split(" ");
+Console.WriteLine(String.Join(";",array));
+ int amount = 0;
+for(int i=0; i<array.Length;i++)
+{
+    int a = Convert.ToInt32(array[i]);
+    if(a>0)
+    {
+        amount++;
+    }
+}
+Console.WriteLine($"Колическтво положительных чисел: {amount}");
